6d27eeb54935ffff267ef2af6e70eb5d8dadd626
Robert Crim <rob@servermilk.com>
2015-10-14 09:56:10 -0700

    Refactor, add readme

--- src/riemann_manifold/core.clj
+++ src/riemann_manifold/core.clj
@@ -1,11 +1,8 @@
 (ns riemann-manifold.core
-  (:require [manifold.stream :as s]
+  (:require [manifold.deferred :as d]
+            [manifold.stream :as s]
             [riemann.client :as r]))
 
-(def c' (r/tcp-client {:host "localhost"}))
-
-(defn- timestamp []
-  (System/currentTimeMillis))
 
 (defn stream-state
   "Gives a string describing the state of a stream."
@@ -23,6 +20,9 @@
   (into {} (filter #(number? (second %))
                    (s/description src))))
 
+
+(defn- timestamp []
+  (quot (System/currentTimeMillis) 1000))
 
 (defn stream-events
   "Returns a collection of Riemann events for a stream. Each numeric
@@ -43,38 +43,76 @@
          (stream-metrics src))))
 
 
-(defn- blocking-sender
-  ([f c ms] (blocking-sender f c 1000 ms))
-  ([f c t ms] (deref (f c ms) t nil)))
+(defprotocol RiemannSender
+  "A lot of hand waving just so we can have a single function to
+  consume metrics from a manifold stream."
+  (send! [m c] [c m t]))
 
-(defn instrument-stream
-  "Send stats from a manifold executor to Riemann. This takes a
-  stream, and puts it onto an executor that will send stats to
+(extend-protocol RiemannSender
+  clojure.lang.PersistentArrayMap
+  (send!
+    ([m c] (send! m c 1000))
+    ([m c t] (deref (r/send-event c m) t nil)))
+
+  clojure.lang.IPersistentCollection
+  (send!
+    ([m c] (send! m c 1000))
+    ([m c t] (deref (r/send-events c m) t nil))))
+
+
+(defn consume-metrics
+  "Take metrics (or a collection of metrics) from a stream, send to
   Riemann."
+  [c src]
+  (s/consume #(send! % c) src))
+
+
+(defn instrument
+  "Send metrics for a manifold stream to Riemann. This will
+  periodically send information provide from
+  `manifold.stream/description` along with a derived state as Riemann
+  events."
 
   [src c service period ttl & tags]
 
-  (let [send! (partial blocking-sender r/send-events c)
-        f (fn []
+  (let [f (fn []
             (stream-events service
                            src
                            ttl
                            (conj tags "manifold-stream")))
         events (s/periodically period f)]
 
-    (s/consume send! events)))
-
+    (consume-metrics c events)))
 
 
 (defn tap
-  "Use the contents of a stream to provide metrics to Riemann."
-  [src c service f ttl & tags]
-  (let [feed (s/stream)
-        send! (partial blocking-sender r/send-event c)
-        wrapf (partial merge {:service service
-                               :ttl ttl
-                               :state (stream-state src)
-                               :time (timestamp)})]
+  "Given a source stream, returns a new \"tapped\" stream that use the
+  contents of a stream to provide metrics to Riemann.
 
-    (s/connect src feed)
-    (s/consume (comp send! wrapf f) feed)))
+  Function `f` should return a map (ideally with a metric key) that
+  will be merged into the event sent to Riemann.
+
+  Tappings a stream is intrusive. This will introduce backpressure
+  where events are sent to Riemann, and set the given stream to be
+  source-only."
+
+  [src c service f ttl & tags]
+
+  (let [sink (s/stream)
+        wrapf (partial merge {:service service
+                              :ttl ttl
+                              :tags tags
+                              :state (stream-state src)
+                              :time (timestamp)})
+
+        tee (s/stream 1 (map (comp wrapf f)))]
+
+    (s/source-only src)
+
+    (s/connect-via src (fn [m]
+                         @(s/put! tee m)
+                         (s/put! sink m)) sink)
+
+    (consume-metrics c tee)
+
+    sink))
